01ebb330d4212f649710f569cbf55d2a69aec497
ojas1888 <ojas.srivastava2003@gmail.com>
2025-10-29 15:43:37 +0530

    CV link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -51,7 +51,7 @@ const About = () => {
 
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1d_eP13W7-P6Bc1mnJyu3cWg0Mj5FjUUm/view?usp=sharing"
+            href="https://drive.google.com/file/d/1crLr2mqnIn9VUuhUyBd_nAcFb9Hs6S54/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
